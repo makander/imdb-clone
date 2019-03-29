a1136f81950c35857f2f08f4cce83e8eaf2cc37c
Nils Makander <makander@gmail.com>
2019-03-29 01:43:14 +0100

    remove links from main view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,6 @@
 @section('content')
 <div>
     <h2>Welcome to diMb</h2>
-    <p>Here you can se our listed <a href="/movies">movies </a>. </p>
-    <p>Here you can se our listed <a href="/series">series</a>.</p>
-    <p>Here you can se our listed <a href="/cast">Cast</a>.</p>
+    <p>Here you can se our listed <a href="/movies">movies </a></p>
 </div>
 @endsection
