bfbd11216b40d32818a5738b88605411b0a19e51
peter heinum <peter.heinum@gmail.com>
2019-04-02 16:43:09 +0200

    detailedview partly finished

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -3,17 +3,15 @@
 
 <div class="main">
     <div class="media" style="display:flex;flex-direction:column;">
-        <div 
-        style="opacity:0.7;
-        background-image: url('https://image.tmdb.org/t/p/original<?php echo $details->backdrop_path?>');
+        <div style="opacity:0.7;
+        background-image: url('https://image.tmdb.org/t/p/original<?php echo $details->backdrop_path ?>');
         height:50vh;
         width:100vw;
         background-attachment: fixed;
         background-position: center;
         background-repeat: no-repeat;
-        background-size: cover;" 
-        ></div>
-        
+        background-size: cover;"></div>
+
         <div style="width:80vw;height:500px; display:flex;">
             <img src="http://image.tmdb.org/t/p/w500//{{$details->poster_path}}" class="align-self-start mr-3" alt="...">
             <div class="media-body jumbotron">
@@ -22,13 +20,10 @@
                 <p>{{$details->overview}}</p><br>
                 <h5>Release Date</h5>
                 <?php echo $details->release_date ?>
-                <h5 style="margin-top:5px;">Rating  <?php echo $details->vote_average ?>/10 </h5>
-               
-
+                <h5 style="margin-top:5px;">Rating <?php echo $details->vote_average ?>/10 </h5>
             </div>
         </div>
     </div>
-</div>
 </div>
 <script>
 
