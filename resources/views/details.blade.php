ace908dd6368651550ca55cf70d24fa366a70b9c
Robin Mossberg <robin.mossberg@chasacademy.se>
2019-04-03 14:25:59 +0200

    added add to Watchlist button to details

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -25,17 +25,32 @@
         </div>
     </div>
     
-@if(auth()->user());
-<div>
-   <form method="POST" action="{{ route('review.create', [$details->id])}}">
-    <!-- action="/movies/{{$details->id}}/review" -->
-    <input type="hidden" name="nickName" value="{{ auth()->user()->nickName }}">
+    @if(auth()->user())
+<div>        
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Add to Watchlist
+        </button>
+        <form method="POST" action="{{ route('movielist.store', [$details->id])}}">
         @csrf
+            <input type="hidden" name="movie_title" value="{{ $details->title }}">
+            <input type="hidden" name="movie_pic" value="{{ $details->poster_path }}">
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                @foreach ($watchlists as $watchlist)
+                    <a class="dropdown-item" href="#"> {{ $watchlist->list_name }}</a>
+                    <input type="hidden" name="list_id" value="{{ $watchlist->id }}">
+                @endforeach
+            </div>
+            <button type="submit">Add</button>
+        </form>
+    </div>
+    <form method="POST" action="/movies/{{$details->id}}/review" placeholder="Dead Link">
+        @csrf
+        <input type="hidden" name="nickName" value="{{ auth()->user()->nickName }}">
 
         <textarea name="content" rows="4" cols="50" /></textarea>
-        <button type="submit">
-            Submit
-        </button>
+        <button type="submit">Submit</button>
     </form>
 </div>
 @endif
