01420a0c503d579aeeb60c2e23c348c90e063966
Nils Makander <makander@gmail.com>
2019-04-11 00:24:01 +0200

    make login, register responsive, make reviews use loongText

--- database/migrations/2019_01_21_145919_create_reviews_table.php
+++ database/migrations/2019_01_21_145919_create_reviews_table.php
@@ -18,7 +18,7 @@ class CreateReviewsTable extends Migration
             $table->integer('movie_id')->unsigned()->notnull();
             $table->integer('author_id')->unsigned()->notnull()->references('id')->on('users');
             $table->string('nickName')->references('nickName')->on('users');
-            $table->string('content');
+            $table->longText('content');
             $table->integer('reviewRating')->nullable()->unsigned();
             $table->boolean('approved')->default(0);
         });
