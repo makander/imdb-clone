6b8777bb38ee4d1e50113a98864855b599d6a7c5
Nils Makander <makander@gmail.com>
2019-04-10 13:11:39 +0200

    create card for genre

--- resources/views/advancedsearch.blade.php
+++ resources/views/advancedsearch.blade.php
@@ -3,11 +3,22 @@
 
 @section('content')
 
-<div class="container">
+<div class="container d-flex flex-wrap p-2 justify-content-center">
 
-    This is the start of the advanced search
+    <div class="card m-2 d-flex justify-content-center" style="width: 18rem;">
+        <div class="card-body text-center">
+            <h5 class="pr-3">Action</h5>
+            <div>
+                <form method="POST" class="form-check form-check-inline" action="">
+                    {{ csrf_field() }}
 
+                    <button type="submit" class="btn btn-outline-success mb-2">
+                        Search</button>
+                    <div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 
-
-@endsection
+    @endsection
